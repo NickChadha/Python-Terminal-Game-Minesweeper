849792eaeba88883056d909248416be3eb850c52
NickChadha <nchadha@purdue.edu>
2024-06-15 22:08:22 -0400

    Implement basic menu logic and flow

--- User.py
+++ User.py
@@ -1,5 +1,40 @@
+import csv
+
 class User:
+    filename = 'user_info.csv'
+
     def __init__(self, username):
         self.username = username
         self.games_won = 0
         self.games_lost = 0
+
+    def check_username(name):
+        while True:
+            if ',' in name:
+                print('Sorry, you can\'t use commas in your username. Please enter a valid username!')
+                name = input()
+            else: # username is valid
+                break
+
+        current_user = None
+        name = name.title()
+        if False: # TODO: check if username exists
+            print('\nWelcome back {NAME}, let\'s play some Minesweeper: Terminal Detonation!'.format(NAME=name))
+            current_user = None
+            # TODO: get user from list
+        else: # new username
+            print('\nLooks like you\'re a new user. Welcome {NAME}, let\'s play some Minesweeper: Terminal Detonation!'.format(NAME=name))
+            current_user = User(name)
+            # TODO: add to user list
+        return current_user
+
+    def view_stats(self):
+        pass   
+
+    @staticmethod 
+    def load_stats():
+        pass
+
+    @staticmethod
+    def write_stats():
+        pass
--- main.py
+++ main.py
@@ -1,18 +1,72 @@
+from User import *
+
 class Main:
     main_menu_options = [
         '1) Read the rules of the game',
-        '2) Sign up',
-        '3) Sign in',
-        '4) Play as a guest'
+        '2) Sign in',
+        '3) Quit'
                          ]
     user_menu_options = [
         '1) Read the rules of the game',
         '2) View my stats',
-        '3) Play a new game'
+        '3) Play a new game',
+        '4) Save and quit'
                             ]
-    def main():
-        print('Minesweeper: Terminal Detonation by Nick Chadha\n')
-        print('Please enter the number indicating which action you would like to take:')
+    def main_menu():
+        choice = 0
+        print('Minesweeper: Terminal Detonation by Nick Chadha')
+        while True:
+            print('\nPlease enter the number indicating the action you would like to take:')
+            for option in Main.main_menu_options:
+                print(option)
+            choice = input('Enter number choice: ').strip()
+            choice = Main.check_choice(choice, len(Main.main_menu_options))
+            if choice == 1: # Read the rules of the game
+                # TODO: implement rules description
+                pass
+            elif choice == 2: # Sign in
+                print('\nPlease enter your username. If you don\'t have one yet, choose any name for your account!')
+                name = input()
+                current_user = User.check_username(name)
+                Main.user_menu(current_user)
+                quit()
+            elif choice == 3: # Quit
+                print('\nThanks for playing!')
+                break
+
+    def user_menu(current_user):
+        while True:
+            print('\nPlease enter the number indicating the action you would like to take:')
+            for option in Main.user_menu_options:
+                print(option)
+            choice = input('Enter number choice: ').strip()
+            choice = Main.check_choice(choice, len(Main.user_menu_options))
+            if choice == 1: # Read the rules of the game
+                # TODO: implement rules description
+                pass
+            elif choice == 2: # View my stats
+                User.view_stats(current_user)
+            elif choice == 3: # Play a new game
+                pass
+            elif choice == 4: # Save and quit
+                # TODO: save
+                print('\nThanks for playing!')
+                return
+            
+
+
+    def check_choice(choice, menu_length):
+        choice = int(choice) # TODO: handle possible error
+        if choice > 0 and choice <= menu_length:
+            return choice
+        else:
+            choice = input('Invalid Input. Please enter a valid number: ').strip()
+            choice = Main.check_choice(choice, menu_length)
+        return choice
+
+def main():
+    User.load_stats()
+    Main.main_menu()
 
 if __name__ == '__main__':
-    Main.main()
+    main()
