c4068119f29f00ffff90ab1eecf7891776a67d55
NickChadha <nchadha@purdue.edu>
2024-06-16 15:35:19 -0400

    Finish implementing add_mines, and added colorama functioning to show_board

--- GameBoard.py
+++ GameBoard.py
@@ -1,12 +1,13 @@
 from colorama import init, Fore, Back, Style
 import random
 
+FORES = [Fore.BLACK, Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
+BACKS = [Back.BLACK, Back.RED, Back.GREEN, Back.YELLOW, Back.BLUE, Back.MAGENTA, Back.CYAN, Back.WHITE]
+BRIGHTNESS = [Style.DIM, Style.NORMAL, Style.BRIGHT]
+
 class Board:
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     init()
-    FORES = [Fore.BLACK, Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
-    BACKS = [Back.BLACK, Back.RED, Back.GREEN, Back.YELLOW, Back.BLUE, Back.MAGENTA, Back.CYAN, Back.WHITE]
-    BRIGHTNESS = [Style.DIM, Style.NORMAL, Style.BRIGHT]
 
     def __init__(self, difficulty):
         self.num_rows = 0
@@ -31,30 +32,46 @@ class Board:
         self.actual_board = [[Tile() for i in range(self.num_cols)] for j in range(self.num_rows)]
     
     def show_board(self):
-        display = ['\n\t\t ']
+        display = ['  ']
         for col in range(self.num_cols):
-            if col == 0:
-                display[0] += '  ' + str(col)
+            if col == 0 or col >= 10:
+                display[0] += '   ' + str(col)
             elif col < 10:
                 display[0] += '    ' + str(col)
-            else:
-                display[0] += '   ' + str(col)
         for row in range(self.num_rows):
-            display.append('\t' + Board.alphabet[row] + '\t')
+            display.append('\t ' + Board.alphabet[row])
             index = row + 1
             for col in range(self.num_cols):
-                display[index] += '  ' + str(self.actual_board[row][col])
+                display[index] += '  [^]'
         
-        fore_index = 0
-        brightness_index = 0
-        for line in display:
-            Board.print_with_color(line, color=Board.FORES[fore_index], brightness=Board.BRIGHTNESS[brightness_index])
-            brightness_index += 1
-            if brightness_index == len(Board.BRIGHTNESS):
-                brightness_index = 0
-                fore_index += 1
+        board_backs = [BACKS[2], BACKS[6]]
+        board_backs_index = 0
+        row_count = 0
+        row_index = 0
+        for line_num in range(len(display)):
+            if line_num == 0:
+                print('\n\t', end='')
+                Board.print_with_color(display[line_num] + ' ', color=BACKS[0], brightness=BRIGHTNESS[2])
+            else:
+                col_index = 0
+                line_split = display[line_num].split('^')
+                Board.print_with_color(line_split[0], color=board_backs[board_backs_index], brightness=BRIGHTNESS[2], end='')
+                for line_split_index in range(len(line_split[1:])):
+                    # check coding style standards for really long lines
+                    Board.print_with_color(self.actual_board[row_index][col_index], color=board_backs[board_backs_index]+Tile.val_to_color[self.actual_board[row_index][col_index].value], brightness=BRIGHTNESS[2], end='')
+                    Board.print_with_color(line_split[line_split_index + 1], color=board_backs[board_backs_index], brightness=BRIGHTNESS[2], end='')
+                    col_index += 1
+                print()
+                row_index += 1
+                row_count += 1
+                if row_count == 3:
+                    row_count = 0
+                    board_backs_index += 1
+                    if board_backs_index == len(board_backs):
+                        board_backs_index = 0
 
     def add_mines(self, first_tile_info):
+        # there's likely a better way to handle this with exceptions
         row = first_tile_info[0]
         col = first_tile_info[1]
         mines_left = self.num_mines
@@ -72,13 +89,30 @@ class Board:
         for safe_row in safe_rows:
             for safe_col in safe_cols:
                 safe_tiles.append([safe_row, safe_col])
-        
         while mines_left > 0:
             mine_location = [random.randint(0, self.num_rows - 1), random.randint(0, self.num_cols - 1)]
             if not mine_location in safe_tiles:
                 self.actual_board[mine_location[0]][mine_location[1]].value = 'X'
                 mines_left -= 1
-        # TODO: finish implementing here
+       
+        for row in range(len(self.actual_board)):
+            for col in range(len(self.actual_board[row])):
+                if self.actual_board[row][col].value == 'X':
+                    affected_rows = [row - 1, row, row + 1]
+                    affected_cols = [col - 1, col, col + 1]
+                    if row == 0:
+                        affected_rows = [row, row + 1]
+                    elif row == self.num_rows - 1:
+                        affected_rows = [row - 1, row]
+                    if col == 0:
+                        affected_cols = [col, col + 1]
+                    elif col == self.num_cols - 1:
+                        affected_cols = [col - 1, col]
+                    for affected_row in affected_rows:
+                        for affected_col in affected_cols:
+                            if not self.actual_board[affected_row][affected_col].value == 'X':
+                                self.actual_board[affected_row][affected_col].value += 1
+        self.uncover_tile(first_tile_info)
         self.show_board()
 
     def uncover_tile(self, tile_info):
@@ -90,12 +124,29 @@ class Board:
     
 
 class Tile:
+    val_to_color = {
+        0: FORES[0], # black
+        1: FORES[5],# magenta
+        2: FORES[3], # yellow
+        3: FORES[7], # white
+        4: FORES[4], # blue
+        5: FORES[5], # magenta
+        6: FORES[3], # yellow
+        7: FORES[7], # white
+        8: FORES[4], # blue
+        'X': FORES[1], # red
+        '*': FORES[1] # red
+    }
+    
     def __init__(self):
         self.visible = True
         self.value = 0
+        self.flagged = False
     
     def __repr__(self):
-        if self.visible:
-            return '[{VALUE}]'.format(VALUE=self.value)
+        if self.flagged:
+            return '*'
+        elif self.visible:
+            return '{VALUE}'.format(VALUE=self.value)
         else:
-            return '[ ]'
+            return ' '
